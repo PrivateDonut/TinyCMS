9a43b86c63ce527b3e1ee37eb593dee81e32c8cf
PrivateDonut <me@privatedonut.com>
2023-05-28 13:24:42 -0700

    Login Check ==================== Added a check so if a user is logging into TinyCMS for the first time, it will insert their account ID into the website->users table to aovid errors.

---
+++ engine/configs/db_config.php
@@ -0,0 +1,14 @@
+<?php 
+// Database Configuration
+$db_host = '127.0.0.1';
+$db_port = '3306';
+$db_username = 'root';
+$db_password = 'ascent';
+$db_auth = 'auth';
+$db_characters = 'characters';
+$db_website = 'website';
+    
+// Soap Account Configuration
+$soap_username = 'admin';
+$soap_password = 'admin';   
+?>
